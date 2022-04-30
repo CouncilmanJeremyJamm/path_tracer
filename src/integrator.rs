e50d53477a8f3704dd52ea4ac0c72d1b35939461
Steadman Slick <vesperalcorn@gmail.com>
2022-04-30 16:11:50 +0100

    Refactored direct lighting estimation
    Split the BSDF sampling step and explicit light sampling step into separate functions, for clarity

--- src/integrator.rs
+++ src/integrator.rs
@@ -7,19 +7,31 @@ use nohash_hasher::IntSet;
 use crate::image_helper::ImageHelper;
 use crate::{BsdfPdf, HitInfo, Material, MaterialTrait, Ray, Scene, Triangle, Volume, ENABLE_NEE, EPSILON, INFINITY};
 
-const MIN_PDF: f32 = 1e-8;
+const MIN_PDF: f32 = 0.0;
+const HEURISTIC_POWER: i32 = 2;
 
 #[inline]
-fn power_heuristic(f: f32, g: f32) -> f32 { (f * f) / (f * f + g * g) }
+/// Heuristic for generating MIS weights
+/// # Parameters
+/// * `f` - pdf of current sampling technique
+/// * `g` - pdf of another sampling technique
+/// * `power` - corresponds to different heuristic functions
+///     * `0`: naive weighting, returns `0.5`
+///     * `1`: balance heuristic
+///     * `2`: power heuristic
+fn mis_heuristic(f: f32, g: f32, power: i32) -> f32 { f.powi(power) / (f.powi(power) + g.powi(power)) }
 
-/// Direct lighting estimation using Multiple-Importance Sampling (MIS).
-/// Rays are generated using two methods:
-/// 1.   Sampled from the material BSDF
-/// 2.   Uniformly sampling a point on a light
-fn estimate_direct(rng: &mut TlsWyRand, bump: &Bump, r: &Ray, hit_info: &HitInfo, mat: &Material, scene: &Scene) -> glam::Vec3A
+/// Direct lighting estimation by explicit light sampling
+fn estimate_direct_explicit(
+    rng: &mut TlsWyRand,
+    bump: &Bump,
+    incoming_ray: &Ray,
+    current_hi: &HitInfo,
+    current_material: &Material,
+    scene: &Scene,
+) -> glam::Vec3A
 {
-    let mut direct: glam::Vec3A = glam::Vec3A::ZERO;
-    let incoming: glam::Vec3A = -r.direction;
+    let incoming: glam::Vec3A = -incoming_ray.direction;
 
     // Pick a random light to sample using the LightSampler in Scene
     // Each light has a pdf proportional to area * ||emitted||
@@ -30,69 +42,102 @@ fn estimate_direct(rng: &mut TlsWyRand, bump: &Bump, r: &Ray, hit_info: &HitInfo
     let (point, light_normal): (glam::Vec3A, glam::Vec3A) = light.random_point(rng);
 
     // Construct ray towards light
-    let o: glam::Vec3A = r.at(hit_info.t);
+    let o: glam::Vec3A = incoming_ray.at(current_hi.t);
     let d: glam::Vec3A = point - o;
 
     let distance_squared: f32 = d.length_squared();
     let distance: f32 = distance_squared.sqrt();
 
-    let light_ray: Ray = Ray::new(o, d / distance);
+    let outgoing_ray: Ray = Ray::new(o, d.normalize());
 
     // Cast shadow ray
     // If light ray points against the normal, occlusion is guaranteed and the shadow ray is skipped
-    if glam::Vec3A::dot(light_ray.direction, hit_info.normal) > 0.0 && !scene.world.any_intersect(bump, &light_ray, (1.0 - EPSILON) * distance)
+    if glam::Vec3A::dot(outgoing_ray.direction, current_hi.normal) > 0.0
+        && !scene.world.any_intersect(bump, &outgoing_ray, (1.0 - EPSILON) * distance)
     {
         // Calculate pdf for current direction, given BSDF sampling
-        let light_info: BsdfPdf = mat.get_bsdf_pdf(incoming, light_ray.direction, hit_info);
+        let bsdf_pdf: BsdfPdf = current_material.get_bsdf_pdf(incoming, outgoing_ray.direction, current_hi);
 
-        if light_info.pdf > MIN_PDF
-        {
-            // Calculate pdf for current direction, given light sampling
-            let cosine: f32 = glam::Vec3A::dot(light_ray.direction, light_normal).abs();
-            let light_pdf: f32 = distance_squared * pdf / (cosine * light.area());
+        let sample_pdf: f32 = pdf / light.area();
 
-            // Calculate weight from heuristic, then accumulate direct light
-            let weight: f32 = power_heuristic(light_pdf, light_info.pdf);
-            direct += light_material.get_emitted() * weight * mat.get_weakening(light_ray.direction, hit_info.normal) * light_info.bsdf / light_pdf;
-        }
+        // Calculate pdf for current direction, given light sampling
+        let cosine: f32 = glam::Vec3A::dot(outgoing_ray.direction, light_normal).abs();
+        let light_pdf: f32 = sample_pdf * (distance_squared / cosine);
+
+        // Calculate weight from heuristic, then accumulate direct light
+        let weight: f32 = mis_heuristic(light_pdf, bsdf_pdf.pdf, HEURISTIC_POWER);
+        return light_material.get_emitted() * weight * current_material.get_weakening(outgoing_ray.direction, current_hi.normal) * bsdf_pdf.bsdf
+            / light_pdf;
     }
 
+    glam::Vec3A::ZERO
+}
+
+/// Direct light estimation by sampling the bsdf
+fn estimate_direct_bsdf(
+    rng: &mut TlsWyRand,
+    bump: &Bump,
+    incoming_ray: &Ray,
+    current_hi: &HitInfo,
+    current_material: &Material,
+    scene: &Scene,
+) -> glam::Vec3A
+{
+    let incoming: glam::Vec3A = -incoming_ray.direction;
+
     //Sample ray from BSDF
-    let material_ray: Ray = Ray::new(o, mat.scatter_direction(rng, r.direction, hit_info.normal, hit_info.front_facing));
+    let outgoing_ray: Ray = Ray::new(
+        incoming_ray.at(current_hi.t),
+        current_material.scatter_direction(rng, incoming_ray.direction, current_hi.normal, current_hi.front_facing),
+    );
 
     // Cast material ray
     // If material ray points against the normal, occlusion is guaranteed and the shadow ray is skipped
-    if glam::Vec3A::dot(material_ray.direction, hit_info.normal) > 0.0
+    if glam::Vec3A::dot(outgoing_ray.direction, current_hi.normal) > 0.0
     {
         // Cheap intersection test with BVH of lights only
         // Checks for a potential light intersection without traversing the full scene BVH
-        if let Some((material_hi, intersected, material)) = scene.lights.intersect(bump, &material_ray, INFINITY)
+        if let Some((light_hi, light, light_material)) = scene.lights.intersect(bump, &outgoing_ray, INFINITY)
         {
             // Cheap test passed, do full shadow ray test
-            if !scene.world.any_intersect(bump, &material_ray, material_hi.t * (1.0 - EPSILON))
+            if !scene.world.any_intersect(bump, &outgoing_ray, light_hi.t * (1.0 - EPSILON))
             {
                 // Calculate pdf for current direction, given BSDF sampling
-                let material_info: BsdfPdf = mat.get_bsdf_pdf(incoming, material_ray.direction, hit_info);
+                let bsdf_pdf: BsdfPdf = current_material.get_bsdf_pdf(incoming, outgoing_ray.direction, current_hi);
 
-                if material_info.pdf > MIN_PDF
+                if bsdf_pdf.pdf > MIN_PDF
                 {
                     // Get pdf for choosing the intersected light
-                    let p: f32 = scene.light_sampler.get_sample_pdf(intersected, material);
+                    let sample_pdf: f32 = scene.light_sampler.get_sample_pdf(light, light_material) / light.area();
 
                     // Calculate pdf for current direction, given light sampling
-                    let cosine: f32 = glam::Vec3A::dot(material_ray.direction, material_hi.normal).abs();
-                    let light_pdf: f32 = material_hi.t * material_hi.t * p / (cosine * intersected.area());
+                    let cosine: f32 = glam::Vec3A::dot(outgoing_ray.direction, light_hi.normal).abs();
+                    let light_pdf: f32 = sample_pdf * (light_hi.t * light_hi.t / cosine);
 
                     // Calculate weight from heuristic, then accumulate direct light
-                    let weight: f32 = power_heuristic(material_info.pdf, light_pdf);
-                    direct += material.get_emitted() * weight * mat.get_weakening(material_ray.direction, hit_info.normal) * material_info.bsdf
-                        / material_info.pdf;
+                    let weight: f32 = mis_heuristic(bsdf_pdf.pdf, light_pdf, HEURISTIC_POWER);
+                    return light_material.get_emitted()
+                        * weight
+                        * current_material.get_weakening(outgoing_ray.direction, current_hi.normal)
+                        * bsdf_pdf.bsdf
+                        / bsdf_pdf.pdf;
                 }
             }
         }
     }
 
-    direct
+    glam::Vec3A::ZERO
+}
+
+/// Direct lighting estimation using Multiple-Importance Sampling (MIS).
+/// Rays are generated using two methods:
+/// 1.   Sampled from the material BSDF
+/// 2.   Uniformly sampling a point on a light
+///
+/// The outputs are then weighted using a heuristic function.
+fn estimate_direct(rng: &mut TlsWyRand, bump: &Bump, r: &Ray, hit_info: &HitInfo, mat: &Material, scene: &Scene) -> glam::Vec3A
+{
+    estimate_direct_explicit(rng, bump, r, hit_info, mat, scene) + estimate_direct_bsdf(rng, bump, r, hit_info, mat, scene)
 }
 
 pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, ImageError>, max_bounces: u32) -> glam::Vec3A
@@ -107,15 +152,15 @@ pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, Ima
 
     let mut volume_stack: IntSet<&Volume> = IntSet::default();
 
-    for b in 0..=max_bounces
+    'trace_ray: for b in 0..=max_bounces
     {
         //Russian roulette
-        if b > 3 || path_weight.max_element() < 1e-10
+        if b > 3
         {
             let survive_prob: f32 = path_weight.max_element().min(0.9999);
             if rng.generate::<f32>() > survive_prob
             {
-                break;
+                break 'trace_ray;
             }
             else
             {
@@ -130,25 +175,19 @@ pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, Ima
             let absorbing = volume_stack.iter().filter_map(|v| v.absorption.as_ref());
             let scattering = volume_stack.iter().filter_map(|v| v.scatter.as_ref());
 
-            let (volume_scattered, dist) = if let Some((t, dir, _)) = scattering
+            if let Some((t, dir, _)) = scattering
                 .filter_map(|s| s.scatter(&mut rng, r.direction, hit_info.t))
                 .min_by(|a, b| a.0.total_cmp(&b.0))
             {
-                r = Ray::new(r.at(t), dir);
+                path_weight *= absorbing.fold(glam::Vec3A::ONE, |w, a| w * a.get_transmission(t));
+                last_delta = true;
 
-                (true, t)
+                r = Ray::new(r.at(t), dir);
+                continue 'trace_ray;
             }
             else
             {
-                (false, hit_info.t)
-            };
-
-            path_weight *= absorbing.fold(glam::Vec3A::ONE, |w, a| w * a.get_transmission(dist));
-
-            if volume_scattered
-            {
-                last_delta = true;
-                continue;
+                path_weight *= absorbing.fold(glam::Vec3A::ONE, |w, a| w * a.get_transmission(hit_info.t));
             }
 
             if material.is_emissive()
@@ -157,7 +196,7 @@ pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, Ima
                 {
                     accumulated += material.get_emitted() * path_weight;
                 }
-                break;
+                break 'trace_ray;
             }
             else
             {
@@ -190,7 +229,7 @@ pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, Ima
                 if material_info.pdf < MIN_PDF
                 {
                     //println!("Invalid PDF, returning early");
-                    break;
+                    break 'trace_ray;
                 }
 
                 path_weight *= material.get_weakening(r.direction, hit_info.normal) * material_info.bsdf / material_info.pdf;
@@ -234,7 +273,7 @@ pub(crate) fn integrate(mut r: Ray, scene: &Scene, env: &Result<ImageHelper, Ima
                 accumulated += glam::Vec3A::new(0.006, 0.006, 0.006) * path_weight;
             }
 
-            break;
+            break 'trace_ray;
         }
     }
 
